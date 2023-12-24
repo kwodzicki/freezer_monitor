6b4f908bbbf76e7bb9168f4854fb16393c611e11
Kyle Wodzicki <krwodzicki@gmail.com>
2023-12-24 03:18:52 +0000

    Adds sensors subpackage
    The sensors subpackage is aimed at expanding the types of temperature
    sensors that can be used by the monitoring package and to enable the
    use of multiple sensors at the same time.

---
+++ freezer_monitor/sensors/basesensor.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This example is for use on (Linux) computers that are using CPython with
+# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+# not support PIL/pillow (python imaging library)!
+import logging
+
+import time
+
+from threading import Thread
+
+import numpy
+
+from . import DATADIR, DEFAULT_INTERVAL, STOP_EVENT, I2C_LOCK, I2C
+from .timerotatingcsv import DailyRotatingCSV
+from .emailer import EMailer
+
+class BaseSensor( EMailer, Thread ):
+
+    def __init__(self, name, max_thres=-10, min_thres=None, interval=DEFAULT_INTERVAL, no_socket=False, **kwargs):
+        """
+        Arguments:
+            name (str) : Name assigned to the sensor. This should be a kind of description
+                about the senors; e.g., location/what it is monitoring
+
+        Keyword arguments:
+            max_thres (int,float) : Temperature threshold (degree C) to trigger warning;
+                if temperature exceeds this value, warning sent. Setting this will override
+                the min_thres keyword
+            min_thres (int,float) : Temperature threshold (degree C) to trigger warning;
+                if temperature is below this value, warning sent. This keyword is ignored
+                if the max_thres keyword is set. 
+            interval (int, float) : Polling interval for sensor (seconds)
+            no_socket (bool) : If set, will disable sending data to server socket
+
+        """
+
+        super().__init__()
+
+        self.__log = logging.getLogger(__name__)
+        self.__log.setLevel( logging.DEBUG )
+
+        self.sensor     = None
+
+        self.name       = name
+        self.interval   = interval
+
+        self.min_thres    = min_thres
+        self.max_thres    = max_thres
+        self.data_log    = DailyRotatingCSV(os.path.join(DATADIR, f"{name}.csv"))
+
+        self.t_30min_avg = numpy.full( int(30*60/interval), numpy.nan, dtype=numpy.float32 )
+
+    def delay(self, t0):
+        """
+        Compute delay until next poll of sensor
+
+        Arguments:
+          t0 (float) : monotonic time of when sensor last polled
+
+        """
+
+        dt = self.interval - (time.monotonic()-t0)																# Compute delay until next poll based on requested interval, current monotonic time, and monotonic time of last poll
+        if dt < 0.0:																															# If the delay is less than zero (0.0), then return zero
+            return 0.0
+        return dt																																	# Return dt
+
+    def _toggle_heater(self, state):
+        """Toggle sensor heater state"""
+
+        with I2C_LOCK:																														# Grab I2C lock for thread safety
+            try:																																		# Try to set the heater state
+                self.sensor.heater = state
+            except Exception as err:																								# On exception, log the a warning
+                self.__log.warning( "Failed to toggle heater : %s", err )
+            else:																																		# Else, some debug logging
+                self.__log.debug( "Heater state set to : %s", state )
---
+++ freezer_monitor/sensors/sht30.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This example is for use on (Linux) computers that are using CPython with
+# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+# not support PIL/pillow (python imaging library)!
+import logging
+
+import time
+
+from threading import Timer, Lock
+
+import numpy
+import adafruit_sht31d
+
+from . import STOP_EVENT, I2C_LOCK, I2C
+from .basesensor import BaseSensor
+
+class SHT30( BaseSensor):
+
+    HEATER_LOCK = Lock()
+
+    def __init__(self, i2c_bus, name, **kwargs):
+        """
+
+        Arguments:
+            i2c_bus (I2C) : An I2C bus the SHT30 sensor is connected to. If using a
+                multiplexer for multiple sensors, this should be the multiplexer
+                channel.
+
+        Keyword arguments:
+            See BaseSensor for keywords
+
+        Example:
+            This is for use of the TCA9548A multiplexer
+
+                tca = adafruit_tca9548a.TCA9548A(i2c)
+                sht30 = SHT30(tca[0], 'Chest Freezer')
+
+        """
+
+        super().__init__(name, **kwargs)
+
+        self.__log = logging.getLogger(__name__)
+        self.__log.setLevel( logging.DEBUG )
+
+        self._heater_timer = None
+        self.sensor        = adafruit_sht31d.SHT31D( i2c_bus )
+
+    def run_heater(self, duration = 10.0):
+        """
+        Run the heater for specified number of seconds
+
+        Keyword arguments:
+            duration (float) : Duration (in seconds) to run heater for
+
+        """
+
+        # Grab lock so that multiple heaters don't run at same time
+        with self.HEATER_LOCK:
+            self._toggle_heater( True )
+            # Wait for STOP_EVENT; if it happens, just return, else, continue function 
+            if STOP_EVENT.wait( duration ):                
+                return
+            self._toggle_heater( False )
+
+        # Initialize and start another timer thread for the heater
+        self._heater_timer = Timer( 30*60, self.run_heater ) 
+        self._heater_timer.start()
+
+    def poll(self):
+        """Poll the sensor for temperature and humidity"""
+
+        t = rh = numpy.nan
+        with I2C_LOCK:																														# Get I2C lock for thread safety
+            try:																																		# Try to get information from temperature sensor
+                t = self.sensor.temperature
+            except Exception as err:
+                self.__log.error(
+                    "Failed to get temperature from sensor : %s",
+                    err,
+                )
+
+            try:																																		# Try to get information from RH sensor
+                rh = self.sensor.relative_humidity
+            except Exception as err:
+                self.__log.error(
+                    "Failed to get relative humidity from sensor : %s",
+                    err,
+                )
+
+        return t, rh																															# Return the temperature and RH
+
+    def run(self):
+        """Run in thread for polling sensor"""
+
+        self._heater_timer = Timer( 0.0, self.run_heater )													  # Initialize timer thread to run heater
+        self._heater_timer.start()																										# Start the timer thread
+
+        t0 = 0.0
+        while not STOP_EVENT.wait( self.delay(t0) ):                              # Wait for event, delay is computed in function and we want event to be NOT set
+            t0       = time.monotonic()# Get current monotonic time; used to compute delay until next poll
+            temp, rh = self.poll()     # Get temperature and humidity
+
+            self.data_log.write( f"{temp:6.1f}", f"{rh:6.1f}" )                         # Write data to the csv file
+
+            self.t_30min_avg     = numpy.roll( self.t_30min_avg, -1 )               # Shift data in rolling averge for new value
+            self.t_30min_avg[-1] = temp                                             # Add new temperature to rolling average array 
+
+            avg = numpy.nanmean( self.t_30min_avg )                                 # Compute average temperature
+            if not numpy.isfinite(avg):                                             # If is not finite
+                self.allNaN()                                                         # AllNaN email
+            elif isinstance(self.max_thres, (int,float)) and (avg > self.max_thres):
+                self.overTemp(temp, rh)                                               # Over temp email
+            elif isinstance(self.min_thres, (int,float)) and (avg < self.min_thres):
+                self.underTemp(temp, rh)                                              # Under temp email
+
+        # If heater timer thread exists, cancel it
+        if self._heater_timer: 
+            self._heater_timer.cancel()
+        # Ensure the heater is turned off
+        self._toggle_heater( False )
+
+        # Close data log and ensure thread is dead
+        self.data_log.close()
+        self.data_log.join()
+
+        self.__log.debug( "Sensor '%s' thread dead!", self.name )
